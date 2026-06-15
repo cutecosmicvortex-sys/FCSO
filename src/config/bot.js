fffb997f3d1b47617f4ad9fb2dc9f3ae3a711f2e
cutecosmicvortex-sys <cutecosmicvortex@gmail.com>
2026-06-14 23:28:56 -0500

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Protecting and serving.",
+        name: "Working hard today, For an better tommorow",
         // Activity type number (0 = Playing).
         type: 0, 
       },
